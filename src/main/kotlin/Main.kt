95815d0ea1f8ae7dd1f8e3c6e15663ef9f076801
Christoph Gierling <christoph.gierling@rewe-digital.com>
2024-08-22 23:48:26 +0200

    Setup kotlin sample kata

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package org.i0.kata
+
+fun main() {
+    println("Hello World!")
+}
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.fail
+
+class MainTest {
+    @Test
+    fun `please give me good name`() {
+        fail()
+    }
+}
