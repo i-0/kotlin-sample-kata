364ba912bfd6258510797d820ebcf876b0bee5c7
Lukas Prediger <lukas.prediger@rewe-digital.com>
2024-09-06 15:22:36 +0200

    Fix package structure

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,19 +1,18 @@
-import org.assertj.core.api.KotlinAssertions.assertThat
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
-import kotlin.test.fail
 
-fun Int.sum10() : Int = this + 10
+fun Int.sum10(): Int = this + 10
 
 class MainTest {
     @Test
     fun `please give me good name`() {
-            assertTrue { true == true }
-        assertThat("foo").isEqualToIgnoringCase("bar")
+        assertTrue { true == true }
+        assertEquals("foo", "bar")
     }
 
     @Test
-    fun `fun adds 10 to a given Int`(){
-        assertThat( 1.sum10()).isEqualTo(11)
+    fun `fun adds 10 to a given Int`() {
+        assertEquals(10, 1.sum10())
     }
 }
